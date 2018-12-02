3b634fb493a2ecf737b0e2c0df6dd3aa6e8bde13
Lukas Burk <lukas@quantenbrot.de>
2018-12-03 00:39:39 +0100

    Init

---
+++ build.R
@@ -0,0 +1,7 @@
+#!/usr/bin/env Rscript
+
+
+#rmarkdown::render(input = "testing.Rmd", output_format = "all")
+rmarkdown::render(input = "testing.Rmd", output_format = "html_document")
+rmarkdown::render(input = "testing.Rmd", output_format = "pdf_document")
+
